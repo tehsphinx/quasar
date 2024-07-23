a556320cdc168939b8c4540d671c88ffc924ec41
Gabriel Nelle <gabriel.nelle@gmail.com>
2024-07-23 12:31:27 +0200

    Add default wait of 10s and limit to 30s

--- injector.go
+++ injector.go
@@ -2,17 +2,27 @@ package quasar
 
 import (
 	"context"
+	"time"
 )
+
+const defaultWait = 5 * time.Second
+const maxWait = 30 * time.Second
 
 type FSMInjector struct {
 	cache *Cache
 }
 
 func (s *FSMInjector) WaitFor(ctx context.Context, uid uint64) error {
+	ctx, cancel := getWaitCtx(ctx)
+	defer cancel()
+
 	return s.cache.fsm.WaitFor(ctx, uid)
 }
 
 func (s *FSMInjector) WaitForMasterLatest(ctx context.Context) error {
+	ctx, cancel := getWaitCtx(ctx)
+	defer cancel()
+
 	// TODO: add context to masterLastIndex
 	uid, err := s.cache.masterLastIndex()
 	if err != nil {
@@ -22,10 +32,20 @@ func (s *FSMInjector) WaitForMasterLatest(ctx context.Context) error {
 }
 
 func (s *FSMInjector) WaitForKnownLatest(ctx context.Context) error {
+	ctx, cancel := getWaitCtx(ctx)
+	defer cancel()
+
 	uid := s.cache.raft.LastIndex()
 	return s.cache.fsm.WaitFor(ctx, uid)
 }
 
 func (s *FSMInjector) Store(bts []byte) (uint64, error) {
 	return s.cache.store("", bts)
+}
+
+func getWaitCtx(ctx context.Context) (context.Context, context.CancelFunc) {
+	if deadline, ok := ctx.Deadline(); ok && time.Until(deadline) < maxWait {
+		return ctx, func() {}
+	}
+	return context.WithTimeout(ctx, defaultWait)
 }
